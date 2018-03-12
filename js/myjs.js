70fcf7abb6fd743e79d95833bb572c952576b003
abhishek-bedarkar <abhishekbedarkar27@gmail.com>
2018-03-12 11:40:27 +0530

    web deployed 11 mar 2018

---
+++ js/myjs.js
@@ -0,0 +1,22 @@
+
+
+function  initImage(){
+    
+   var init="images/office.jpg";
+    return init;
+    changeImage();
+    
+}
+
+function changeImage(){
+    var img="images/2.jpg";
+    return  img;
+    changeImage1();
+}
+
+function changeImage1(){
+    var img2="images/3.jpeg";
+   return img2;
+    initImage();
+    
+}
